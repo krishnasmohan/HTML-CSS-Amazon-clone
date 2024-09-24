b78e98f890f8bc7cc0061ab4deeea5e3e6f64aea
krishnasmohan <168458211+krishnasmohan@users.noreply.github.com>
2024-09-24 12:26:00 +0530

    Initial Commit

---
+++ product.js
@@ -0,0 +1,33 @@
+export const products =[{
+    image:'assets/box1-1.jpg',
+    name:'Free international returns',
+    a:'Shop more'
+},{
+    image:'assets/box1-2.jpg',
+    name:'Lunar New Year',
+    a:'Shop more'
+},{
+    image:'assets/box1-3.jpg',
+    name:'Toy under $25',
+    a:'Shop more'
+},{
+    image:'assets/box1-4.jpg',
+    name:'Deals in PCs',
+    a:'Shop more'
+},{
+    image:'assets/box2-1.jpg',
+    name:'Grooming Products',
+    a:'Shop more'
+},{
+    image:'assets/box2-2.jpg',
+    name:'Latest Devices',
+    a:'Shop more'
+},{
+    image:'assets/box2-3.jpg',
+    name:'Pets Food',
+    a:'Shop more'
+},{
+    image:'assets/box2-4.jpg',
+    name:'Fashoin Mart',
+    a:'Shop more'
+}]
